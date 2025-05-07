c3c64d6b5a74764c610690caa91b252c1bf96fa7
Tornesko <kostiantyn.borovenskyi@raccoongang.com>
2025-05-07 03:30:20 +0300

    refactor

--- app/schemas/transaction.py
+++ app/schemas/transaction.py
@@ -1,57 +1,53 @@
 import enum
+from datetime import datetime
+from decimal import Decimal
 from typing import Optional, List
 
 from pydantic import BaseModel, ConfigDict, HttpUrl
-from datetime import datetime
-from decimal import Decimal
 
 
-class TransactionStatus(str, enum.Enum):
+# === ENUMS ===
+class StatusEnum(str, enum.Enum):
     NEW = "NEW"
     PENDING = "PENDING"
     PROCESSED = "PROCESSED"
     FAIL = "FAIL"
 
 
-class TransactionRead(BaseModel):
-    tx_hash: str
-    amount: Decimal
-    confirmations: int
-    status: TransactionStatus
-    created_at: datetime
-
+class ORMBaseModel(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
 
+# Transactions
 class IncomingTransaction(BaseModel):
     wallet_address: str
     tx_hash: str
-    amount: float
+    amount: Decimal
 
 
-class WalletStatus(str, enum.Enum):
-    NEW = "NEW"
-    PENDING = "PENDING"
-    PROCESSED = "PROCESSED"
-    FAIL = "FAIL"
+class TransactionRead(ORMBaseModel):
+    tx_hash: str
+    amount: Decimal
+    confirmations: int
+    status: StatusEnum
+    created_at: datetime
 
 
+# Wallets
 class WalletCreate(BaseModel):
     currency: str
     network: str
     callback_url: Optional[HttpUrl] = None
 
 
-class WalletRead(BaseModel):
+class WalletRead(ORMBaseModel):
     id: int
     address: str
     currency: str
     network: str
-    status: WalletStatus
+    status: StatusEnum
     created_at: datetime
     callback_url: Optional[HttpUrl] = None
-
-    model_config = ConfigDict(from_attributes=True)
 
 
 class WalletDetail(WalletRead):
